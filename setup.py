747c6cb20a8a5e394ea26fbc97b9d1acacc930f0
Maxim Litvak <maxim.litvak@gmail.com>
2021-07-11 18:59:10 +0200

    adapted setup.py for gitlab

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+import os
 
 long_description = """pyEdgeworthBox calculates concepts of the pure exchange economy
 (subject to microeconomics) and draws the Edgeworth box
 """
 
 setup(name='pyEdgeworthBox',
-    version='0.1.1',
+    version=os.environ['CI_COMMIT_TAG'],
     description='Python Library to draw the Edgeworth box and calculate equilibrium, core, pareto efficient allocations etc',
     author='Maxim Litvak',
     author_email='maxim@litvak.eu',
-    url='http://github.com/maxlit/pyEdgeworthBox',
+    url='https://gitlab.com/maxlit/pyEdgeworthBox',
     test_suite='test',
     packages=['pyEdgeworthBox'],
     classifiers=[
