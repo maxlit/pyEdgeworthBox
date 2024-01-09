f8e7c4eeadc52ce4ed5a07ec475d4d12023f19ea
Maxim Litvak <maxim.litvak@gmail.com>
2024-01-09 18:18:08 +0100

    fixing build setup

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# Fetch all tags and ensure they are up to date
+git fetch --tags
+
+# Get the latest tag
 VERSION=$(git tag --sort=-creatordate | head -n 1)
+
+# Update the version in pyproject.toml
 poetry version $VERSION
+
+# Now run poetry build
 poetry build
